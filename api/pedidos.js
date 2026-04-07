523d6de826bd8d27d058197ae9b7ed43880ba400
ThalesRuntrix <tech@runtrix.com.br>
2026-04-07 18:23:17 +0000

    debug payload

--- api/pedidos.js
+++ api/pedidos.js
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
 
   try {
     const payload = formatarPedidoPayload(req.body);
+    console.warn("PAYLOAD: ", payload);
 
     const { cliente, endereco, itens, pagamento, frete, prazo, entrega } = payload;
 
