767252467b936e03bf6be68b4f591a037145b189
ThalesRuntrix <tech@runtrix.com.br>
2026-04-07 14:21:01 +0000

    bugfix: CORS

--- api/pedidos.js
+++ api/pedidos.js
@@ -6,6 +6,10 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
   }
@@ -43,5 +47,5 @@ export default async function handler(req, res) {
     console.error("Erro geral:", err);
     return res.status(500).json({ error: "Erro interno" });
   }
-  
+
 }
