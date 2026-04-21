894eab42c0b4c0ca38521de509b6acbf267eacc0
ThalesRuntrix <tech@runtrix.com.br>
2026-04-21 14:21:43 +0000

    feat: add cors config

--- api/payment/pix.js
+++ api/payment/pix.js
@@ -1,7 +1,7 @@
 import mercadopago from "../../lib/mercadoPago.js";
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://runtrix.com.br");
   res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
