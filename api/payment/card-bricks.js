ad54c16fbc27937755cedfb083b298d6f9124740
ThalesRuntrix <tech@runtrix.com.br>
2026-04-27 17:42:57 +0000

    remove unused /card to get mor limit in vercel

--- api/payment/card-bricks.js
+++ api/payment/card-bricks.js
@@ -7,7 +7,8 @@ export default async function handler(req, res) {
 
   res.setHeader("Access-Control-Allow-Origin", "https://runtrix.com.br");
   res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Max-Age", "86400");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
